0ec7f4a142eb62eb14d0c814fffc80a2f8951620
techminds <aztechminds@gmail.com>
2024-12-20 21:48:41 +0100

    added function fetchAlbumData

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+
+// Log to confirm script is loaded
 console.log("Script loaded");
 console.log("Das ist der zweite Aufruf");
+
+// References from HTML-Elements
 const userlist = document.getElementById('user-list');
 const imageList = document.getElementById('image-list');
+const albumList = document.getElementById('album-list');
 
 
 // Define an array for users
-let users = []; 
+let users = [];
 let images = []; // to be filled with images from api endpoint https://jsonplaceholder.typicode.com/photos
+let albums = [];
 
 // define async function to fetch users data
-async function fetchUserData(){
+async function fetchUserData() {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         // console.log(typeof(response);
@@ -18,7 +24,7 @@ async function fetchUserData(){
         users = usersData;
         console.log(users);
         renderUsers();
-        
+
     } catch (error) {
         console.log("Wir bekommen beim Aufruf der Users-APi den folgenden Fehler", error);
     }
@@ -36,13 +42,33 @@ async function fetchImageData() {
     } catch (error) {
         console.log("Wir bekommen beim Fetching der Image API folgenden Fehler: ", error)
     }
-    
+
 }
 
+// define async function to fetch Albums
+
+async function fetchAlbumData() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+        const albumData = await response.json();
+        albums = albumData.slice(0, 10);
+        console.log(albums);
+        renderAlbums();
+    } catch (error) {
+        console.log("Error by fetching Album Data: ", error);
+
+    }
+
+}
+
+// Funktion aufrufen
 fetchUserData();
 fetchImageData();
+fetchAlbumData();
 
-function renderUsers(){
+
+// Rendering function for user
+function renderUsers() {
     users.forEach((user) => {
         const userItem = document.createElement('li');
         userItem.innerHTML = user.name;
@@ -52,7 +78,8 @@ function renderUsers(){
     });
 }
 
-function renderImages(){
+// Rendering function for image
+function renderImages() {
     images.forEach((image) => {
         const imageItem = document.createElement('img');
         console.log(imageItem);
@@ -61,5 +88,16 @@ function renderImages(){
 
     });
 }
+
+// Rendering function for album
+function renderAlbums() {
+    albums.forEach((album) => {
+        const albumItem = document.createElement('li');
+        albumItem.innerHTML = album.title;
+        albumList.appendChild(albumItem);
+
+    });
+}
+
 
 // showUsers();
